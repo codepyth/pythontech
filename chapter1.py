e583f70c3c37d9fdade9cf7da0de5352bc41b14a
codepyth <waseempy@gmail.com>
2026-04-09 10:23:57 +0100

    finding a largest number

--- chapter1.py
+++ chapter1.py
@@ -1,10 +1,25 @@
-print("Ya Allah meri madad Farma")
-
-databc = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
-
-counter = 1
-for i in databc:
-    print(f"{counter} = {i}: {ord(i)}")
-    counter = counter + 1
+# print("Ya Allah meri madad Farma")
+#
+# databc = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+#
+# counter = 1
+# for i in databc:
+#     print(f"{counter} = {i}: {ord(i)}")
+#     counter = counter + 1
 
 # Uploaded the questioner
+
+
+# Let's rewrite to check the largest number
+import pdb
+numbers = [23, 43, 54, 2, 89, 90, 44]
+
+largest = numbers[0]
+
+for i in numbers:
+    # breakpoint()
+
+    if i > largest:
+        largest = i
+
+print(largest)
