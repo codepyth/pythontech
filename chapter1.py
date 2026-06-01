c9e0bc2335dd5179af4a3a77528e79c401c1d07a
codepyth <waseempy@gmail.com>
2026-06-01 20:31:33 +0100

    procedural programming top to bottom functions writing and usage

--- chapter1.py
+++ chapter1.py
@@ -12,14 +12,37 @@
 
 # Let's rewrite to check the largest number
 import pdb
+
 numbers = [23, 43, 54, 2, 89, 90, 44]
 
 largest = numbers[0]
 
-for i in numbers:
-    # breakpoint()
 
-    if i > largest:
-        largest = i
+# for i in numbers:
+#     # breakpoint()
+#
+#     if i > largest:
+#         largest = i
+#
+# print(largest)
 
-print(largest)
+
+# Let's understand Procedural vs Object-oriented programming
+
+# Procedural: we write functions step by step and use them top to bottom
+
+def get_user():
+    return "I'm"
+
+
+def get_food():
+    return "Typing my order down"
+
+
+def place_order():
+    person = get_user()
+    food = get_food()
+    return print(f"Hi, {person} {food}")
+
+
+place_order()
