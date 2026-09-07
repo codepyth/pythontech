8dceda52142ca6df472a17a552e8e9ab571adca4
codepyth <waseempy@gmail.com>
2026-09-07 14:05:54 +0100

    some basics

--- chapter1.py
+++ chapter1.py
@@ -33,15 +33,99 @@ largest = numbers[0]
 def get_user():
     return "I'm"
 
+#
+# def get_food():
+#     return "Typing my order now"
+#
+#
+# def place_order():
+#     person = get_user()
+#     food = get_food()
+#     return print(f"Hi, {person} {food}")
+#
+#
+# place_order()
 
-def get_food():
-    return "Typing my order now"
+#
+# mystring = "Hello"
+# myfloat = 4.863
+# myint = 28
+#
+# if mystring == "Hello":
+#     print("String: %s" % mystring)
+#
+# if isinstance(myfloat, float) and myfloat == 4.863:
+#     print("Float: %f" % myfloat)
+#
+# if isinstance(myint, int) and myint == 28:
+#     print("Integer: %d" % myint)
+#
+#
+# print(...)
 
 
-def place_order():
-    person = get_user()
-    food = get_food()
-    return print(f"Hi, {person} {food}")
+# print("Last print")
 
 
-place_order()
+
+# mylist = [1,2,3]
+# print(mylist[2])
+
+
+
+# even_numbers = [2,4,6,8]
+# odd_numbers = [1,3,5,7]
+# all_numbers = even_numbers, [odd_numbers]
+# print(all_numbers)
+
+
+
+# TODO: change this code
+
+# x = object()
+# y = object()
+
+
+# print(x)
+# x_list = [x]
+# y_list = [y]
+# big_list = [2,5]
+#
+# print("x_list contains %d objects" % len(x_list))
+# print("y_list contains %d objects" % len(y_list))
+# print("big_list contains %d objects" % len(big_list))
+#
+#
+# print(big_list.count(x))
+# # testing code
+# if x_list.count(x) == 1 and y_list.count(y) == 1:
+#     print("Both lists have the same count of Items")
+# if big_list.count(x) == 0 and big_list.count(y) == 0:
+#     print("Great!")
+
+
+# name = "John"
+# print("Hello, %s!" % name)
+
+
+# name = "John"
+# age = 23
+# print("%d is %s years old." % (age, name))
+
+
+# mylist = [1,2,"uiu"]
+#
+#
+# print("A list: %s" % mylist)
+
+
+# data = ["John", "Doe", 53.44]
+# format_string = "Hello"
+#
+# print(format_string % data)
+
+
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
